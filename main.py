669eed73fffe5defd8cfa9aef08cf33565c57f03
Siavosh_Mozafari_nezhad <siavoshmozafary@gmail.com>
2022-09-05 12:56:33 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,60 @@
+import random
+
+
+def roll_dice():
+    dice_drawing = {
+        1: (
+            "-----",
+            "|   |",
+            "| o |",
+            "|   |",
+            "-----",
+        ),
+        2: (
+            "-----",
+            "|o  |",
+            "|   |",
+            "|  o|",
+            "-----",
+        ),
+        3: (
+            "-----",
+            "|o  |",
+            "| o |",
+            "|  o|",
+            "-----",
+        ),
+        4: (
+            "-----",
+            "|o o|",
+            "|   |",
+            "|o o|",
+            "-----",
+        ),
+        5: (
+            "-----",
+            "|o o|",
+            "| o |",
+            "|o o|",
+            "-----",
+        ),
+        6: (
+            "-----",
+            "|o o|",
+            "|o o|",
+            "|o o|",
+            "-----",
+        ),
+
+    }
+    roll = input("Do yot want to play dice? (Yes/No) : ")
+    while roll.lower() == "Yes".lower():
+        dice = random.randint(1, 6)
+
+        print("dice rolled: {}".format(dice))
+        print("\n".join(dice_drawing[dice]))
+
+        roll = input("Roll again? (Yes/no): ")
+
+
+roll_dice()
